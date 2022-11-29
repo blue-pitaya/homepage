79754edbfc224d90dab47f6165ee254ddbced7ba
blue-pitaya <blue.pitaya@pm.me>
2022-11-29 03:08:54 +0100

    added made using qwik note

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,11 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import styles from "./styles.css?inline";
 
 export default component$(() => {
+  useStylesScoped$(styles);
+
   return (
-    <div class="home">
+    <div>
       <h1>Home</h1>
       <p>
         Hi! I'm blue pitaya, aka kodus8 and this is my homepage. I post here
@@ -11,13 +14,26 @@ export default component$(() => {
         shaggy's reversed face appearing from corner, i put it for fun and no
         other reason.
       </p>
-      <p>This website won't work properly on phones, since i hate css.</p>
+      <p>
+        This website won't work properly on phones, since i'm too lazy to write
+        few lines of css. ¯\_(ツ)_/¯
+      </p>
       <p>
         Contact me, if you want:{" "}
         <span class="blueAddress">blue.pitaya@pm.me</span>
         <br />
-        My github page:{" "}<a href="https://github.com/blue-pitaya">blue-pitaya</a>
+        My github page: <a href="https://github.com/blue-pitaya">blue-pitaya</a>
       </p>
+      <div class="madeUsingContainer">
+        <p>This page was made using:</p>
+        <a href="https://qwik.builder.io/">
+          <img class="qwikLogo" src="/images/qwik-logo.svg" />
+        </a>
+        <p>
+          because of{" "}
+          <a href="https://www.youtube.com/watch?v=x2eF3YLiNhY">this</a> hype.
+        </p>
+      </div>
     </div>
   );
 });
