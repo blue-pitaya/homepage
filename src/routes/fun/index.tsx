8d454aaccd009ad376b4e37997c53a3e88e0f4c7
blue-pitaya <blue.pitaya@pm.me>
2022-12-10 19:25:20 +0100

    added links to good content

--- src/routes/fun/index.tsx
+++ src/routes/fun/index.tsx
@@ -16,16 +16,32 @@ export default component$(() => {
 
   return (
     <div class="fun-container">
-      <noscript>This section needs javascript!</noscript>
-      <h2 class="colored">Random video from my important videos collection.</h2>
-      <YtIframe videoId={getVideoId(state.videoNum)} />
-      <p class="centered">
-        <span id="imp-video-label">{getLabel(state.videoNum)}</span>
+      <div class="impVidsContainer">
+        <noscript>This section needs javascript!</noscript>
+        <h2 class="colored">
+          Random video from my important videos collection.
+        </h2>
+        <YtIframe videoId={getVideoId(state.videoNum)} />
+        <p class="centered">
+          <span id="imp-video-label">{getLabel(state.videoNum)}</span>
+          <br />
+          <a class="reloadLink" onClick$={reload}>
+            Pray to RNG gods for another video
+          </a>
+        </p>
+      </div>
+      <h2>Links to good content</h2>
+      <div class="linksCont">
+        <h3>Music</h3>
+        <a href="https://nightride.fm/stations">nightride.fm</a> - synths radio
         <br />
-        <a class="reloadLink" onClick$={reload}>
-          Pray to RNG gods for another video
-        </a>
-      </p>
+        <h3>Web 1.0</h3>
+        <a href="https://wiby.me/">wiby.me</a> - minimal/oldschool websites
+        search engine
+        <br />
+        <a href="https://neocities.org/browse">neocities.org</a> - oldschool
+        website hosting with list of sites
+      </div>
     </div>
   );
 });
