1d099fb875475c1de7d4c7d153a0ae6e8a3fb053
blue-pitaya <blue.pitaya@pm.me>
2022-12-16 19:28:24 +0100

    updated fun links

--- src/routes/fun/index.tsx
+++ src/routes/fun/index.tsx
@@ -33,14 +33,42 @@ export default component$(() => {
       <h2>Links to good content</h2>
       <div class="linksCont">
         <h3>Music</h3>
-        <a href="https://nightride.fm/stations">nightride.fm</a> - synths radio
-        <br />
+        <ul>
+          <li>
+            <a href="https://nightride.fm/stations">nightride.fm</a> - Synth
+            music radio. Tune in to high-quality, 320kbps electronic music
+            including Synthwave, Cyberpunk, Darksynth, EBSM, Midtempo,
+            Industrial, Dark Techno, Drum & Bass genres and much more.
+          </li>
+          <li>
+            <a href="https://www.generationdiscofunk.com/">
+              generationdiscofunk.com
+            </a>{" "}
+            (
+            <a href="https://www.generationdiscofunk.com/player/gdf/">player</a>
+            ) - Soul, disco & funk radio.
+          </li>
+          <li>
+            <a href="https://everynoise.com/">everynoise.com</a> - Every Noise
+            at Once is an ongoing attempt at an algorithmically-generated,
+            readability-adjusted scatter-plot of the musical genre-space, based
+            on data tracked and analyzed for 5,996 genre-shaped distinctions by
+            Spotify as of 2022-12-15.
+          </li>
+        </ul>
         <h3>Web 1.0</h3>
-        <a href="https://wiby.me/">wiby.me</a> - minimal/oldschool websites
-        search engine
+        <ul>
+          <li>
+            <a href="https://wiby.me/">wiby.me</a> - Minimal/oldschool websites
+            search engine. You can find true gems of internet here.
+          </li>
+
+          <li>
+            <a href="https://neocities.org/browse">neocities.org</a> - Oldschool
+            website hosting with list of sites.
+          </li>
+        </ul>
         <br />
-        <a href="https://neocities.org/browse">neocities.org</a> - oldschool
-        website hosting with list of sites
       </div>
     </div>
   );
